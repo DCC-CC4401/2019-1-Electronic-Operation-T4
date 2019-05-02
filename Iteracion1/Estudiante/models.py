c310fc0daf545059c356f451df389b26b9af7c45
Claudio Mallea <claudiomallea.a@gmail.com>
2019-05-02 15:46:36 -0400

    Agregado Equipo (model.py y admin.py)

--- Iteracion1/Estudiante/models.py
+++ Iteracion1/Estudiante/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Estudiante(models.Model):
+
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    historial          = models.CharField(max_length=20)
+    Presentó        =    models.BooleanField(default=False)
+    nombre        = models.CharField(max_length=20)
+    Notas        = models.CharField(max_length=20)
+
