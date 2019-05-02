64aa0a262b9a5f01391e5e2d826df9756333bcd1
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-02 15:51:52 -0400

    TODO: Revisar implementación de ForeignKey

--- Iteracion1/Equipo/models.py
+++ Iteracion1/Equipo/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
+#from ..Curso.models import Curso
+
 # Create your models here.
+class Equipo(models.Model):
+    id       = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    #id_curso = models.ForeignKey('Curso', on_delete=models.CASCADE, blank=False, null=False)
+    número   = models.PositiveSmallIntegerField(blank=False, null=False, default=1)
+    nombre   = models.CharField(blank=False, null=False, default='Grupo 1', max_length=100)
+    #class Meta:
+    #    unique_together = [['id_curso', 'número']]
