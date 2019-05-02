36f813b96f077a5fa5c126744fd3cd2541bc216a
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-02 14:58:07 -0400

    Prohibición de subir campos en blanco

--- Iteracion1/Curso/models.py
+++ Iteracion1/Curso/models.py
@@ -9,10 +9,10 @@ class Curso(models.Model):
         (2, 'Primavera'),
         (3, 'Verano')
     )
-    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    código          = models.CharField(max_length=20)
-    número_seccion  = models.PositiveSmallIntegerField(default=1)
-    año             = models.IntegerField(default=dt.now().year)
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    código          = models.CharField(blank=False, null=False, max_length=20)
+    número_seccion  = models.PositiveSmallIntegerField(blank=False, null=False, default=1)
+    año             = models.IntegerField(blank=False, null=False, default=dt.now().year)
     semestre        = models.SmallIntegerField(choices=SEMESTRE_CHOICES, default=1)
     class Meta:
         unique_together = [['código', 'número_seccion', 'año', 'semestre']]
