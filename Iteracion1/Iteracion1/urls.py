e0cce13947cf8c8f247a6d4ca19f9db12110f614
nikom1912 <nikom_1912@hotmail.com>
2019-05-02 17:45:41 -0400

    Agregadas algunas views

--- Iteracion1/Iteracion1/urls.py
+++ Iteracion1/Iteracion1/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view,landing_evaluaciones_view,ficha_rubrica_admin_view
+from pages.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view),
     path('landing1/',landing_evaluaciones_view),
-    path('ficha-rubrica/',ficha_rubrica_admin_view)
+    path('ficha-rubrica/',ficha_rubrica_admin_view),
+    path('ficha-evaluacion/', ficha_evaluacion_admin_view)
 ]
--- Iteracion1/pages/views.py
+++ Iteracion1/pages/views.py
@@ -6,4 +6,13 @@ def home_view(request,*args,**kwargs):
 def landing_evaluaciones_view(request,*args,**kwargs):
      return render(request,"Admin-landing/admin_evaluaciones_gestion.html",{})
 def ficha_rubrica_admin_view(request,*args,**kwargs):
-     return render(request,"Ficha-evaluaciones/post_evaluacion_admin.html",{})
+     return render(request,"Ficha-rubricas/ficha_rubrica_admin.html",{})
+
+def ficha_rubrica_evaluador_view(requesr, *arg, **kwargs):
+     return render(request, "Fichas-rubricas/ficha_rubrica_evaluador.html", {})
+def landing_evaluadores_view(request, *arg, **kwargs):
+     return render(request, "Admin-landing/admin_evaluadores_gestion.html", {})
+def landing_rubricas_view(request, *arg, **kwargs):
+     return render(request, "Admin-landing/admin_rubricas_gestion.html", {})
+def ficha_evaluacion_admin_view(request, *arg, **kwargs):
+     return render(request, "Ficha-evaluaciones/ficha_evaluacion_admin.html", {})
