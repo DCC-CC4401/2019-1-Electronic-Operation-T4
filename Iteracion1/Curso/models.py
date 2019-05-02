9d39c984f2dc6a8e93e6e1a971a831e299c8c825
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-02 14:33:05 -0400

    Implementado modelo de curso

--- Iteracion1/Curso/models.py
+++ Iteracion1/Curso/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    SEMESTRE_CHOICES = (
+        (1, 'Otoño'),
+        (2, 'Primavera'),
+        (3, 'Verano')
+    )
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    código          = models.CharField(max_length=20)
+    número_seccion  = models.PositiveSmallIntegerField(default=1)
+    año             = models.IntegerField()
+    semestre        = models.SmallIntegerField(choices=SEMESTRE_CHOICES, default=1)
+    class Meta:
+        unique_together = [['código', 'número_seccion', 'año', 'semestre']]
