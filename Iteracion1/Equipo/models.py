f7da202fe24eaa2ef90663e9c6c5bf304394abef
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-04 22:50:42 -0400

    Agregado criterio de unicidad

--- Iteracion1/Equipo/models.py
+++ Iteracion1/Equipo/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Equipo(models.Model):
     id       = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-    id_curso = models.ForeignKey('Curso.Curso', on_delete=models.CASCADE, blank=False, null=False)
+    id_Curso = models.ForeignKey('Curso.Curso', on_delete=models.CASCADE, blank=False, null=False)
     número   = models.PositiveSmallIntegerField(blank=False, null=False, default=1)
     nombre   = models.CharField(blank=False, null=False, default='Grupo 1', max_length=100)
-    #class Meta:
-    #    unique_together = [['id_curso', 'número']]
+    class Meta:
+        unique_together = [['id_curso', 'número']]
