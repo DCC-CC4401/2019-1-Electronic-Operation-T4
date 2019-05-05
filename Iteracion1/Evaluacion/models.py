c32a7f6057465c9b00ff7902103c347bd39086ca
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-05 01:08:36 -0400

    Implementado modelo de evaluacion

--- Iteracion1/Evaluacion/models.py
+++ Iteracion1/Evaluacion/models.py
@@ -1,3 +1,10 @@
+import uuid
+from django.utils import timezone as tz
 from django.db import models
 
 # Create your models here.
+class Evaluacion(models.Model):
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fecha_Inicio    = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
+    fecha_Fin       = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
+    is_Open         = models.BooleanField(default=True)
