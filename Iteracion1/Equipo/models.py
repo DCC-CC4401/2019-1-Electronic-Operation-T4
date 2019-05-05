0b0e41f2ad2c77759d9ef359722cc5a72e72a905
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-04 22:43:54 -0400

    Solucionado error de ForeignKey

--- Iteracion1/Equipo/models.py
+++ Iteracion1/Equipo/models.py
@@ -1,12 +1,10 @@
 import uuid
 from django.db import models
 
-#from ..Curso.models import Curso
-
 # Create your models here.
 class Equipo(models.Model):
     id       = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-    #id_curso = models.ForeignKey('Curso', on_delete=models.CASCADE, blank=False, null=False)
+    id_curso = models.ForeignKey('Curso.Curso', on_delete=models.CASCADE, blank=False, null=False)
     número   = models.PositiveSmallIntegerField(blank=False, null=False, default=1)
     nombre   = models.CharField(blank=False, null=False, default='Grupo 1', max_length=100)
     #class Meta:
