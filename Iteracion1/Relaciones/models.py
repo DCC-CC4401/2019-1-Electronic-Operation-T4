7c2ada057f9b3a1c6e20585b2182e2b7d429a524
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-07 16:03:07 -0400

    Establecidas relaciones

--- Iteracion1/Relaciones/models.py
+++ Iteracion1/Relaciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Usuario_Evaluacion(models.Model):
+    id_Usuario      = models.ForeignKey('Usuario.Usuario', on_delete=models.CASCADE, blank=False, null=False)
+    id_Evaluación   = models.ForeignKey('Evaluacion.Evaluacion', on_delete=models.CASCADE, blank=False, null=False)
+
+class Evaluacion_Equipo(models.Model):
+    id_Evaluación   = models.ForeignKey('Evaluacion.Evaluacion', on_delete=models.CASCADE, blank=False, null=False)
+    id_Equipo       = models.ForeignKey('Equipo.Equipo', on_delete=models.CASCADE, blank=False, null=False)
+
+class Rubrica_Resumen(models.Model):
+    id_Rúbrica = models.ForeignKey('Rubrica.Rubrica', on_delete=models.CASCADE, blank=False, null=False)
+    id_Resumen = models.ForeignKey('Resumen.Resumen', on_delete=models.CASCADE, blank=False, null=False)
