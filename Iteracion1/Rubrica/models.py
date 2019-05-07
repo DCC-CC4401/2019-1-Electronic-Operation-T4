7fcc5d035bfa657663185706fa7443da3cb0aed3
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-07 15:17:15 -0400

    Agregado nombre de rúbrica

--- Iteracion1/Rubrica/models.py
+++ Iteracion1/Rubrica/models.py
@@ -5,6 +5,9 @@ from django.db import models
 # Create your models here.
 class Rubrica(models.Model):
     id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre          = models.CharField(blamk=False, default='Nombre de rúbrica', max_length=100)
     rúbrica         = models.FileField(upload_to=None, max_length=100)
     duración_Mínima = models.TimeField(blank=False, null=False, default=tz.now())
     duración_Máxima = models.TimeField(blank=False, null=False, default=tz.now())
+    class Meta:
+        unique_together = [['id', 'nombre']]
