a921a39c7b10e4ba757f5c96a77c927bacbb7538
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-09 16:03:10 -0400

    Formulario adaptado a los requisitos

--- Iteracion1/Usuario/forms.py
+++ Iteracion1/Usuario/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserModel
 
+from .models import Usuario
 
 class RegistroUsuarioForm(UserCreationForm):
-    email=forms.EmailField()
-
     class Meta:
         model = User
-        fields  = ['email', 'password1', 'password2', 'first_name', 'last_name']
+        fields  = ['email','first_name', 'last_name']
