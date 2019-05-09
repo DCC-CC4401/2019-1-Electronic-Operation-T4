86f967b57783e0272b40f03e99747412fdd43e7b
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-09 16:03:36 -0400

    Generación de contraseña y correo como username

--- Iteracion1/Usuario/views.py
+++ Iteracion1/Usuario/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .forms import RegistroUsuarioForm
 
 # Create your views here.
 
 def registro(request, *arg, **kwargs):
+     constaseña = ""
      if request.method == 'POST':          
           form = RegistroUsuarioForm(request.POST)
-          if form.is_valid():
-               #username = email o guardar en modelo Usuario              
-               form.save()
-               NombreUsuario=form.cleaned_data.get('email')
+          if form.is_valid():       
+               usuario = form.save(commit=False)
+               contraseña = User.objects.make_random_password()
+               usuario.set_password(contraseña)
+               usuario.username = usuario.email
+               usuario.save()
      else:
           form = RegistroUsuarioForm()
      for key, value in kwargs.items():
           if key == 'path':
                path = value
-     return render(request, path, {'form' : form})
+     return render(request, path, {'form' : form, 'contraseña' : contraseña})
