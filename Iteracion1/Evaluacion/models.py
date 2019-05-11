ae88fbe9994724de98d6bf925ea589b4e12f16f8
nikom1912 <nikom_1912@hotmail.com>
2019-05-11 18:00:43 -0400

    Agregado nombre

--- Iteracion1/Evaluacion/models.py
+++ Iteracion1/Evaluacion/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 class Evaluacion(models.Model):
     id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre          = models.CharField(blank=False, default='Evaluacion default', max_length=100)
     fecha_Inicio    = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
     fecha_Fin       = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
     is_Open         = models.BooleanField(default=True)
