028128090a57cae04e66ceda0d241a0d26f3b87c
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-14 16:38:17 -0400

    Recuperado modelo usuario

--- Iteracion1/Usuario/models.py
+++ Iteracion1/Usuario/models.py
@@ -2,3 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Usuario(models.Model):
+    correo_Electrónico  = models.CharField(blank=False, null=False, max_length=50)
+    nombre              = models.CharField(blank=False, null=False, max_length=30)
+    apellido            = models.CharField(blank=False, null=False, max_length=30)
+    contraseña          = models.CharField(blank=False, null=False, max_length=64)
+    es_Admin            = models.BooleanField(blank=False, null=False, default=False)
