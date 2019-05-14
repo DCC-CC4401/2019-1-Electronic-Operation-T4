d2834b12ad7125678da7995787432538dc43e699
nikom1912 <nikom_1912@hotmail.com>
2019-05-14 16:23:55 -0400

    Arreglo error al no table Nombre_Curso

--- Iteracion1/Evaluacion/forms.py
+++ Iteracion1/Evaluacion/forms.py
@@ -3,6 +3,7 @@ from Relaciones.models import Evaluacion_Curso, Evaluacion_Rubrica
 from Curso.models import Curso
 from Rubrica.models import Rubrica
 from Nombre_Curso.models import Nombre_Curso
+from django.db.utils import OperationalError
 
 
 class CreateFormEvaluacion(forms.Form):
@@ -25,17 +26,28 @@ class CreateFormEvaluacion(forms.Form):
         'id':'fecha-fin',
         'type': 'Date'
     }))
-    nombre_cursos = Nombre_Curso.objects.all()
-    curso = forms.ChoiceField(widget=forms.Select({
-         'name':'curso-evaluacion',
-         'id':'curso'
-     }), choices=((x.id_Curso.id, str(x.id_Curso.código) + "-" +
-                                    str(x.id_Curso.número_sección) + " " +
-                                    str(x.Nombre) + " " +
-                                    str(x.id_Curso.año) + "-" +
-                                    str(x.id_Curso.semestre)) for x in nombre_cursos))
-    rubrica_select = Rubrica.objects.all()
-    rubrica = forms.ChoiceField(widget=forms.Select({
-         'name':'rubrica-evaluacion',
-         'id':'rubrica'
-     }), choices=((x.id, x.nombre ) for x in rubrica_select))
+    try:
+        nombre_cursos = Nombre_Curso.objects.all()
+        curso = forms.ChoiceField(widget=forms.Select({
+            'name':'curso-evaluacion',
+            'id':'curso'
+        }), choices=((x.id_Curso.id, str(x.id_Curso.código) + "-" +
+                                        str(x.id_Curso.número_sección) + " " +
+                                        str(x.Nombre) + " " +
+                                        str(x.id_Curso.año) + "-" +
+                                        str(x.id_Curso.semestre)) for x in nombre_cursos))
+        
+        rubrica = forms.ChoiceField(widget=forms.Select({
+            'name':'rubrica-evaluacion',
+            'id':'rubrica'
+        }), choices=((x.id, x.nombre ) for x in rubrica_select))
+    except OperationalError:
+        rubrica = forms.ChoiceField(widget=forms.Select({
+            'name':'rubrica-evaluacion',
+            'id':'rubrica'
+        }))
+        curso = forms.ChoiceField(widget=forms.Select({
+            'name':'curso-evaluacion',
+            'id':'curso'
+        }))
+    
