d6b42e1f485d34ec289cd8b0c0a65ce96da8be31
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-17 15:43:31 -0400

    Vista formulario de modificación deplazo

--- Iteracion1/Evaluacion/views.py
+++ Iteracion1/Evaluacion/views.py
@@ -5,4 +5,14 @@ from .forms import ActualizarPlazoForm
 # Create your views here.
 
 def actualizarPlazo(request, *args, **kwargs):
-    pass
+    if request.method == 'POST':
+        form = ActualizarPlazoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ActualizarPlazoForm()
+    for key, value in kwargs.items():
+        if key == 'path':
+            path = value
+    return render(request, path, {'form' : form})
+    #Implementar en conjunto con frontend
