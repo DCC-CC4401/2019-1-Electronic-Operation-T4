be879b8f29a8ff199d3504ddf579b7f1e2278fdf
Clemente Paredes <clparede@dcc.uchile.cl>
2019-05-29 18:42:36 -0400

    Home_view requiere login

--- Iteracion1/pages/views.py
+++ Iteracion1/pages/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url="/login/")
 def home_view(request,*args,**kwargs):
      return render(request,"Usuario/landing.html",{})
 
