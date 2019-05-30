c62cda940217cb5a5b7aa7edb4e9b8e121e75505
Nicolás Machuca <nikom_1912@hotmail.com>
2019-05-30 15:55:23 -0400

    Cambios en nombres campos

--- Iteracion1/Relaciones/models.py
+++ Iteracion1/Relaciones/models.py
@@ -14,9 +14,9 @@ class Rubrica_Resumen(models.Model):
     id_Resumen = models.ForeignKey('Resumen_Evaluacion.Resumen_Evaluacion', on_delete=models.CASCADE, blank=False, null=False)
 
 class Evaluacion_Curso(models.Model):
-    id_Evaluacion = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
+    id_Evaluación = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
     id_Curso = models.ForeignKey('Curso.Curso', on_delete=models.CASCADE, blank=False, null=False)
 
 class Evaluacion_Rubrica(models.Model):
-    id_Evaluacion = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
+    id_Evaluación = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
     id_Rúbrica = models.ForeignKey('Rubrica.Rubrica', on_delete=models.CASCADE, blank=False, null=False)
