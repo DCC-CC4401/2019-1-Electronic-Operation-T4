3494496706acafad4a9efc4a16dd38c6bd75c2a4
nikom1912 <nikom_1912@hotmail.com>
2019-06-02 17:00:59 -0400

    Agregado formulario para agregar presentadores

--- Iteracion1/Relaciones/forms.py
+++ Iteracion1/Relaciones/forms.py
@@ -3,12 +3,18 @@ from Usuario.models import Usuario
 from django.db.utils import OperationalError
 from .models import Usuario_Evaluacion
 
+"""
+Formulario para agregar evaluadores a una evaluacion
 
+
+TODO: Revisar si se puede cambiar por modelForm
+"""
 class FormUsuarioEnEvaluacion(forms.Form):
-    evaluador = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple({
+    evaluadores = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple({
                 'name': 'select-evaluadores',
-                'id' : 'select_evaluador'
-            }))
+                'id' : 'select_evaluador',
+                'class' :'w3-ul w3-border'
+            }), required=False)
     def __init__(self,*args,**kwargs):
         id_evaluacion = kwargs.pop('id_evaluacion')
         super(FormUsuarioEnEvaluacion, self).__init__(*args,**kwargs)
@@ -16,9 +22,28 @@ class FormUsuarioEnEvaluacion(forms.Form):
             usuarios_evaluando = Usuario_Evaluacion.objects.filter(id_Evaluación=id_evaluacion)
             correos = ((x.id_Usuario.correo_Electrónico) for x in usuarios_evaluando)
             users = Usuario.objects.all().exclude(correo_Electrónico__in=correos)
-            self.fields['evaluador'].choices = ((x.id, x.nombre + " " + x.apellido) for x in users)
+            self.fields['evaluadores'].choices = ((x.id, x.nombre + " " + x.apellido) for x in users)
         except OperationalError:
-            self.fields['evaluador'].choices = []
-    
+            self.fields['evaluadores'].choices = []
+
+
+class FormAgregarPresentador(forms.Form):
+    presentadores = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple({
+                'name': 'select-presentador',
+                'id' : 'select_presentador',
+                'class' :'w3-ul w3-border'
+            }), required=False)
+    def __init__(self,*args,**kwargs):
+        evaluacion = kwargs.pop('evaluacion')
+        equipo = kwargs.pop('equipo')
+        super(FormAgregarPresentador, self).__init__(*args,**kwargs)
+        try:
+            estudiantes_presentando = Evaluacion_Estudiante.objects.filter(id_Evaluación=evaluacion)
+
+            ids = ((x.id_Estudiante.id) for x in estudiantes_presentando)
+            students = Estudiante.objects.filter(id_Equipo=equipo).exclude(id__in=ids)
+            self.fields['presentadores'].choices = ((x.id, x.Nombre) for x in students)
+        except OperationalError:
+            self.fields['presentadores'].choices = []
     
     
