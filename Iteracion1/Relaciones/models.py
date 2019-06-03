b2dda47e2f1105021ff816b93425665149e5a8c3
nikom1912 <nikom_1912@hotmail.com>
2019-06-03 12:58:53 -0400

    Agregado relacion estudiante-eval y triple

--- Iteracion1/Relaciones/models.py
+++ Iteracion1/Relaciones/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import json
 
 # Create your models here.
 class Usuario_Evaluacion(models.Model):
-    id_Usuario      = models.ForeignKey('Usuario.Usuario', on_delete=models.CASCADE, blank=False, null=False)
+    id_Usuario      = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     id_Evaluación   = models.ForeignKey('Evaluacion.Evaluacion', on_delete=models.CASCADE, blank=False, null=False)
 
 class Evaluacion_Equipo(models.Model):
@@ -32,3 +34,26 @@ Author:
 class Evaluacion_Estudiante(models.Model):
     id_Evaluación = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
     id_Estudiante = models.ForeignKey('Estudiante.Estudiante', on_delete=models.CASCADE, blank=False, null=False)
+
+
+"""
+Modelo de la relacion Evaluacion_Equipo_Usuario para la base de datos
+Fields:
+    id_Evaluacion   : (UUID) Identificador de la evaluación en curso
+    id_Equipo       : (UUID) Identificador del equipo evaluado
+    id_Usuario      : (Char) Identificador del evaluador (email)
+    puntajes        : (Char) Puntajes asignados al equipo por el usuario en la evaluacion
+Author:
+Nicolás Machuca
+"""
+class Evaluacion_Equipo_Usuario(models.Model):
+    id_Evaluación = models.ForeignKey('Evaluacion.Evaluacion',  on_delete=models.CASCADE, blank=False, null=False)
+    id_Equipo     = models.ForeignKey('Equipo.Equipo', on_delete=models.CASCADE, blank=False, null=False)
+    id_Usuario    = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    puntajes      = models.CharField(blank=False, default='[]', max_length = 120)
+
+    def get_puntajes(self):
+        return json.loads(self.puntajes)
+    
+    def set_puntajes(self, list):
+        self.puntajes = json.dumps(list)
