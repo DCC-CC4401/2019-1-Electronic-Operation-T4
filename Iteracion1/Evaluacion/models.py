6e84df5177a923bdc247604e1a6a882bb850d5a2
nikom1912 <nikom_1912@hotmail.com>
2019-06-03 12:54:33 -0400

    Agregados Fields: equipo,editable, curso

--- Iteracion1/Evaluacion/models.py
+++ Iteracion1/Evaluacion/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 # Create your models here.
 class Evaluacion(models.Model):
-    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nombre          = models.CharField(blank=False, default='Evaluacion default', max_length=100)
-    fecha_Inicio    = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
-    fecha_Fin       = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
-    is_Open         = models.BooleanField(default=True)
+    id                  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_Curso            = models.ForeignKey('Curso.Curso', on_delete=models.CASCADE, blank=False, null=True)
+    nombre              = models.CharField(blank=False, default='Evaluacion default', max_length=100)
+    fecha_Inicio        = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
+    fecha_Fin           = models.DateField(auto_now=False, auto_now_add=False, default=tz.now())
+    is_Open             = models.BooleanField(default=True)
+    is_Editable         = models.BooleanField(default=True)
+    equipo_Presentando  = models.ForeignKey('Equipo.Equipo', on_delete=models.SET_NULL, blank=False, null=True)
